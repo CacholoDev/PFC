32686136c65e8250caa0a514485431c40c06c28c
CacholoDev <adriannoia104@gmail.com>
2026-02-11 21:20:33 +0100

    falta excep de negocio --> services

--- PFC/pfcdaw/src/main/java/com/pfcdaw/pfcdaw/config/ExceptionHandlerGlobal.java
+++ PFC/pfcdaw/src/main/java/com/pfcdaw/pfcdaw/config/ExceptionHandlerGlobal.java
@@ -3,8 +3,12 @@ package com.pfcdaw.pfcdaw.config;
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 
+import javax.naming.AuthenticationException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -61,4 +65,82 @@ public class ExceptionHandlerGlobal {
 
         return ResponseEntity.status(status).body(body);
     }
-}
+
+    // excep personalizada
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<GlobalExceptionHandlerDTO> handleRuntimeException(
+            RuntimeException ex,
+            HttpServletRequest request) {
+                HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        
+                GlobalExceptionHandlerDTO body = GlobalExceptionHandlerDTO.builder()
+                        .timeStamp(OffsetDateTime.now().format(formatter))
+                        .status(status.value())
+                        .error(status.getReasonPhrase())
+                        .message("An unexpected error occurred: " + ex.getMessage())
+                        .path(request.getRequestURI())
+                        .build();
+        
+                return ResponseEntity.status(status).body(body);
+    }
+
+    // excep personalizada para acceso no autentificado (401)
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<GlobalExceptionHandlerDTO> handleAuthenticationException(
+            AuthenticationException ex,
+            HttpServletRequest request) {
+
+        HttpStatus status = HttpStatus.UNAUTHORIZED;
+
+        GlobalExceptionHandlerDTO body = GlobalExceptionHandlerDTO.builder()
+                .timeStamp(OffsetDateTime.now().format(formatter))
+                .status(status.value())
+                .error(status.getReasonPhrase())
+                .message("Authentication failed: " + ex.getMessage())
+                .path(request.getRequestURI())
+                .build();
+
+        return ResponseEntity.status(status).body(body);
+    }
+
+    // excep personalizada para acceso denegado (403)
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<GlobalExceptionHandlerDTO> handleAccessDeniedException(
+            AccessDeniedException ex,
+            HttpServletRequest request) {
+
+        HttpStatus status = HttpStatus.FORBIDDEN;
+
+        GlobalExceptionHandlerDTO body = GlobalExceptionHandlerDTO.builder()
+                .timeStamp(OffsetDateTime.now().format(formatter))
+                .status(status.value())
+                .error(status.getReasonPhrase())
+                .message("Access denied: " + ex.getMessage())
+                .path(request.getRequestURI())
+                .build();
+
+        return ResponseEntity.status(status).body(body);
+    }
+
+    //json invalido
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<GlobalExceptionHandlerDTO> handleHttpMessageNotReadableException(
+            HttpMessageNotReadableException ex,
+            HttpServletRequest request) {
+
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        GlobalExceptionHandlerDTO body = GlobalExceptionHandlerDTO.builder()
+                .timeStamp(OffsetDateTime.now().format(formatter))
+                .status(status.value())
+                .error(status.getReasonPhrase())
+                .message("Invalid JSON: " + ex.getMessage())
+                .path(request.getRequestURI())
+                .build();
+
+        return ResponseEntity.status(status).body(body);
+    }
+
+    // Negocio ( excepciones desde services)
+    
+}       
